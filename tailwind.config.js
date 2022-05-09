feda2ab4e67017ceea77c400141ecc32d67cd11d
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2022-05-09 15:36:03 -0300

    criação de funcionalidade camera, comunicação entre componentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
+const tailwindcss = require("tailwindcss");
+
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       colors: {
         brand: {
+          300: '#996dff',
           500: '#8257e6',
+
         }
+      },
+      borderRadius: {
+        md:'4px',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
